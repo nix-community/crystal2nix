d18a71c3b64604e4920c7b97bd3793992af2180d
vidhvath28 <vidhvathj.2005@gmail.com>
2024-06-15 10:10:51 +0530

    update

--- src/worker.cr
+++ src/worker.cr
@@ -15,34 +15,33 @@ module Crystal2Nix
             exit 1
           end
           sha256 = ""
-          case 
+          case
           when repo.url.ends_with?(".git")
-          
-          args = [
-            "--no-deepClone",
-            "--url", repo.url,
-            "--rev", repo.rev,
-          ]
-          Process.run("nix-prefetch-git", args: args) do |x|
-            x.error.each_line { |e| puts e }
-            sha256 = PrefetchJSON.from_json(x.output).sha256
+            args = [
+              "--no-deepClone",
+              "--url", repo.url,
+              "--rev", repo.rev,
+            ]
+            Process.run("nix-prefetch-git", args: args) do |x|
+              x.error.each_line { |e| puts e }
+              sha256 = PrefetchJSON.from_json(x.output).sha256
+            end
+          when repo.url.starts_with?("https://hg.") || repo.url.ends_with?(".hg")
+            args = [
+              "--url", repo.url,
+              "--rev", repo.rev,
+            ]
+            Process.run("nix-prefetch-hg", args: args) do |x|
+              x.error.each_line { |e| puts e }
+              sha256 = PrefetchJSON.from_json(x.output).sha256
+            end
+          when repo.url.ends_with?(".fossil")
+            STDERR.puts "Fossil repositories are not supported."
+            next
+          else
+            STDERR.puts "Unknown repository type for #{repo.url}"
+            next
           end
-        when repo.url.starts_with?("hg://") || repo.url.ends_with?(".hg")
-          args = [
-            "--url", repo.url,
-            "--rev", repo.rev,
-          ]
-          Process.run("nix-prefetch-hg", args: args) do |x|
-            x.error.each_line { |e| puts e }
-            sha256 = PrefetchJSON.from_json(x.output).sha256
-          end
-        when repo.url.ends_with?(".fossil")
-          STDERR.puts "Fossil repositories are not supported."
-          next
-        else
-          STDERR.puts "Unknown repository type for #{repo.url}"
-          next
-        end
 
           file.puts %(  #{key} = {)
           file.puts %(    url = "#{repo.url}";)
