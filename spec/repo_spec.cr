30726917f4532482cafd8cc9128bd48e6bf8f378
vidhvath28 <vidhvathj.2005@gmail.com>
2024-06-16 11:31:16 +0530

    Milestone achieved

--- spec/repo_spec.cr
+++ spec/repo_spec.cr
@@ -1,85 +1,32 @@
-require "./spec_helper"
+require "spec"  # Import the spec module from the Crystal standard library
+require "../src/crystal2nix"  # Adjust the path according to your project structure
 
-Spectator.describe Crystal2Nix::Repo do
-  context "commit" do
-    let(:with_commit) {
-      <<-EOF
-      git: https://github.com/cadmiumcr/transliterator.git
-      version: 0.1.0+git.commit.46c4c14594057dbcfaf27e7e7c8c164d3f0ce3f1
-      EOF
-    }
-
-    let(:shard) {
-      Crystal2Nix::Shard.from_yaml(with_commit)
-    }
-
-    let(:repo) {
-      Crystal2Nix::Repo.new(shard.url, shard.rev, shard.type)
-    }
-
-    it "should have the commit as revision" do
-      expect(repo.rev).to eq("46c4c14594057dbcfaf27e7e7c8c164d3f0ce3f1")
-    end
+describe Crystal2Nix::Repo do
+  it "creates a new repo with a URL and default values" do
+    repo = Crystal2Nix::Repo.new("https://example.com/repo.git")
+    repo.url.should eq("https://example.com/repo.git")
+    repo.rev.should be_nil
+    repo.type.should eq(:git)
   end
 
-  context "explicit version" do
-    let(:with_version) {
-      <<-EOF
-      git: https://github.com/crystal-lang/json_mapping.cr.git
-      version: 0.1.1
-      EOF
-    }
-
-    let(:shard) {
-      Crystal2Nix::Shard.from_yaml(with_version)
-    }
-
-    let(:repo) {
-      Crystal2Nix::Repo.new(shard.url, shard.rev, shard.type)
-    }
-
-    it "should prefix version references with a v" do
-      expect(repo.rev).to eq("v0.1.1")
-    end
+  it "creates a new repo with a URL, rev, and type" do
+    repo = Crystal2Nix::Repo.new("https://example.com/repo.git", "master", :git)
+    repo.url.should eq("https://example.com/repo.git")
+    repo.rev.should eq("master")
+    repo.type.should eq(:git)
   end
 
-  context "http url" do
-    let(:with_http_url) {
-      <<-EOF
-      http: https://example.com/archive.tar.gz
-      EOF
-    }
-
-    let(:shard) {
-      Crystal2Nix::Shard.from_yaml(with_http_url)
-    }
-
-    let(:repo) {
-      Crystal2Nix::Repo.new(shard.url, shard.rev, shard.type)
-    }
-
-    it "should have the correct url" do
-      expect(repo.url).to eq("https://example.com/archive.tar.gz")
-    end
+  it "creates a new repo with a URL and rev only" do
+    repo = Crystal2Nix::Repo.new("https://example.com/repo.git", "develop")
+    repo.url.should eq("https://example.com/repo.git")
+    repo.rev.should eq("develop")
+    repo.type.should eq(:git)  # Default type is :git
   end
 
-  context "git without revision" do
-    let(:without_revision) {
-      <<-EOF
-      git: https://github.com/example/repo.git
-      EOF
-    }
-
-    let(:shard) {
-      Crystal2Nix::Shard.from_yaml(without_revision)
-    }
-
-    let(:repo) {
-      Crystal2Nix::Repo.new(shard.url, shard.rev, shard.type)
-    }
-
-    it "should have the correct url" do
-      expect(repo.url).to eq("https://github.com/example/repo.git")
-    end
+  it "creates a new repo with a URL and type only" do
+    repo = Crystal2Nix::Repo.new("https://example.com/repo.git", nil, :svn)
+    repo.url.should eq("https://example.com/repo.git")
+    repo.rev.should be_nil
+    repo.type.should eq(:svn)
   end
 end
