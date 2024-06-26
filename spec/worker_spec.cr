d1a5fdec1eb45244f614c78492547cbb53be3be5
vidhvath <vidhvath@Ubuntu.myguest.virtualbox.org>
2024-06-26 12:18:16 +0530

    integrated test case added 5

--- spec/worker_spec.cr
+++ spec/worker_spec.cr
@@ -30,5 +30,104 @@ YAML
       File.delete(lock_file)
       File.delete(Crystal2Nix::Worker::SHARDS_NIX)
     end
+
+    it "shards.nix file contains correct content" do
+      lock_file = "test_lock_file.lock"
+      File.write(lock_file, <<-YAML)
+version: 1.0
+shards:
+  example_shard:
+    git: https://github.com/example/example.git
+    version: v1.0.0
+YAML
+
+      expected_nix_content = <<-NIX
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.stdenv.mkDerivation {
+  name = "example_shard";
+  src = pkgs.fetchFromGitHub {
+    owner = "example";
+    repo = "example";
+    rev = "v1.0.0";
+    sha256 = "0000000000000000000000000000000000000000000000000000";
+  };
+}
+NIX
+
+      worker = Crystal2Nix::Worker.new(lock_file)
+      worker.run
+
+      actual_nix_content = File.read(Crystal2Nix::Worker::SHARDS_NIX)
+      expect(actual_nix_content).to eq expected_nix_content
+
+      # Clean up the temporary files
+      File.delete(lock_file)
+      File.delete(Crystal2Nix::Worker::SHARDS_NIX)
+    end
+
+    it "handles multiple shards and creates correct shards.nix file" do
+      lock_file = "test_lock_file.lock"
+      File.write(lock_file, <<-YAML)
+version: 1.0
+shards:
+  example_shard:
+    git: https://github.com/example/example.git
+    version: v1.0.0
+  another_shard:
+    git: https://github.com/another/example.git
+    version: v2.0.0
+YAML
+
+      expected_nix_content = <<-NIX
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.stdenv.mkDerivation {
+  name = "example_shard";
+  src = pkgs.fetchFromGitHub {
+    owner = "example";
+    repo = "example";
+    rev = "v1.0.0";
+    sha256 = "0000000000000000000000000000000000000000000000000000";
+  };
+}
+
+pkgs.stdenv.mkDerivation {
+  name = "another_shard";
+  src = pkgs.fetchFromGitHub {
+    owner = "another";
+    repo = "example";
+    rev = "v2.0.0";
+    sha256 = "0000000000000000000000000000000000000000000000000000";
+  };
+}
+NIX
+
+      worker = Crystal2Nix::Worker.new(lock_file)
+      worker.run
+
+      actual_nix_content = File.read(Crystal2Nix::Worker::SHARDS_NIX)
+      expect(actual_nix_content).to eq expected_nix_content
+
+      # Clean up the temporary files
+      File.delete(lock_file)
+      File.delete(Crystal2Nix::Worker::SHARDS_NIX)
+    end
+
+    it "handles an empty lock file and creates an empty shards.nix file" do
+      lock_file = "empty_lock_file.lock"
+      File.write(lock_file, <<-YAML)
+version: 1.0
+shards: {}
+YAML
+
+      worker = Crystal2Nix::Worker.new(lock_file)
+      worker.run
+
+      expect(File.exists?(Crystal2Nix::Worker::SHARDS_NIX)).to be_true
+      expect(File.read(Crystal2Nix::Worker::SHARDS_NIX)).to eq ""
+
+      # Clean up the temporary files
+      File.delete(lock_file)
+      File.delete(Crystal2Nix::Worker::SHARDS_NIX)
+    end
   end
 end
