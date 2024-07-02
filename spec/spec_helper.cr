9fc5d139371b2c16f43c4085e627415df35d31b6
Vidhvath J <vidhvathj.2005@gmail.com>
2024-07-02 19:09:35 +0530

    checks

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,23 +1,2 @@
 require "../src/crystal2nix"
 require "spectator"
-
-module SpecHelper
-    def self.verify_shards_nix_content(expected_content : Hash(String, Hash(String, String)))
-      content = File.read("shards.nix")
-      expected_content.each do |name, details|
-        content.should contain("\"name\": \"#{name}\"")
-        content.should contain("\"version\": \"#{details["version"]}\"")
-        content.should contain("\"github\": \"#{details["repository"].gsub("https://github.com/", "").gsub(".git", "")}\"")
-      end
-    end
-  
-    def self.create_shard_lock(dependencies : Hash(String, Hash(String, String)))
-      content = { "dependencies" => dependencies }.to_json
-      File.write("shard.lock", content)
-    end
-  
-    def self.run_crystal2nix
-      system("crystal2nix --lock-file=shard.lock > shards.nix")
-    end
-  end
-  
