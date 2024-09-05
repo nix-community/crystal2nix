c059007673f21a7d4e4dfcf53987dc6427cdb24e
vidhvath28 <vidhvathj.2005@gmail.com>
2024-09-05 16:52:42 +0530

    Added Mercurial(hg) source

--- src/repo.cr
+++ src/repo.cr
@@ -2,10 +2,23 @@ module Crystal2Nix
   class Repo
     @url : URI
     getter rev : String
+    getter type : Symbol
 
     def initialize(entry : Shard)
-      @url = URI.parse(entry.git).normalize
-      @rev = if entry.version =~ /^(?<version>.+)\+git\.commit\.(?<rev>.+)$/
+      git_url = entry.git.try(&.not_nil!)
+      hg_url = entry.hg.try(&.not_nil!)
+
+      if git_url
+        @url = URI.parse(git_url)
+        @type = :git
+      elsif hg_url
+        @url = URI.parse(hg_url)
+        @type = :hg
+      else
+        raise "Unknown repository type"
+      end
+
+      @rev = if entry.version =~ /(?<version>.+)\+(git|hg)\.commit\.(?<rev>.+)/
                $~["rev"]
              else
                "v#{entry.version}"
