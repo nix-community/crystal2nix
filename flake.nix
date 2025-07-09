41bae80ca8ab99b8668ea44ef11712b2d40a15c8
Peter H. Hoeg <peter@hoeg.com>
2025-07-09 21:40:26 +0800

    feat: use shards version to get the version

--- flake.nix
+++ flake.nix
@@ -6,8 +6,14 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    (with flake-utils.lib; eachSystem defaultSystems) (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    (with flake-utils.lib; eachSystem defaultSystems) (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
@@ -16,7 +22,7 @@
         packages = flake-utils.lib.flattenTree rec {
           crystal2nix = pkgs.crystal.buildCrystalPackage {
             pname = "crystal2nix";
-            version = "0.3.0";
+            inherit (pkgs.lib.importJSON ./version.json) version;
 
             src = ./.;
 
@@ -49,5 +55,6 @@
             ];
           };
         };
-      });
+      }
+    );
 }
--- src/crystal2nix.cr
+++ src/crystal2nix.cr
@@ -7,12 +7,8 @@ require "json"
 require "yaml"
 require "option_parser"
 require "uri"
-require "version_from_shard"
 
 require "./data"
 require "./repo"
+require "./version"
 require "./worker"
-
-module Crystal2Nix
-  VersionFromShard.declare
-end
