1a2b470c7278f02c7c78b8ccb91fa3a11c8a724e
Peter H. Hoeg <peter@hoeg.com>
2025-07-09 21:36:55 +0800

    release v0.3.1

--- src/crystal2nix.cr
+++ src/crystal2nix.cr
@@ -1,8 +1,3 @@
-# This module defines the Crystal2Nix project, which converts Crystal project dependencies 
-# into Nix expressions for use with the Nix package manager. The project leverages various 
-# dependencies such as JSON, YAML, URI parsing, and a custom version management module. 
-# The main functionality is organized into separate components like data handling, 
-# repository management, and worker processes.
 require "json"
 require "yaml"
 require "option_parser"
